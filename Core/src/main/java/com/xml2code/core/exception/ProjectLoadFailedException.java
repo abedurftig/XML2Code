cc26c945273878f315319a3d0e1cc4934f349538
dasnervtdoch <Arne@Arnes-MacBook-Pro.local>
2014-05-23 19:29:49 +0100

    Created Core project entry path

---
+++ Core/src/main/java/com/xml2code/core/exception/ProjectLoadFailedException.java
@@ -0,0 +1,14 @@
+package com.xml2code.core.exception;
+
+public class ProjectLoadFailedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ProjectLoadFailedException(String message) {
+		super(message);
+	}
+
+}
---
+++ Core/src/main/java/com/xml2code/core/loader/ProjectLoader.java
@@ -0,0 +1,15 @@
+package com.xml2code.core.loader;
+
+import com.xml2code.core.definition.ProjectDef;
+import com.xml2code.core.exception.ProjectLoadFailedException;
+
+public class ProjectLoader {
+
+	public static ProjectDef loadProject(String projectFilePath) throws ProjectLoadFailedException {
+		
+		// TODO: implement
+		return null;
+		
+	}
+	
+}
