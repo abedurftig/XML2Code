fd1626b88bc7ada833ec7edac85b705d55be3f49
dasnervtdoch <Arne@Arnes-MacBook-Pro.local>
2014-06-27 22:30:17 +0100

    traces

--- Application/src/main/java/com/xml2code/application/Main.java
+++ Application/src/main/java/com/xml2code/application/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String projectFolder = "/Users/Arne/Workspaces/Languageschool";
+		String projectFolder = "";
 		
 		try {
 			
